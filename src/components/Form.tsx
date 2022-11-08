bcf9104273ccabf602d4151769575bd09f3b1015
nashvador <nashvador@gmail.com>
2022-11-08 00:33:13 -0500

    Remove nested block from Form page

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -61,81 +61,74 @@ const FormPage = () => {
   const handleChange = (event: SelectChangeEvent) => {
     setOccupationValue(event.target.value as string);
   };
-  {
-    return loading ? (
-      <CircularProgress />
-    ) : (
-      <Box component="form" onSubmit={handleSubmit} maxWidth="sm">
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="name"
-          label="Name"
-          name="name"
-          autoComplete="name"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <FormControl fullWidth sx={{ mt: 2 }}>
-          <InputLabel id="occupation-select">Occupation</InputLabel>
-          <Select
-            labelId="occupation-select"
-            id="frontend-occupation-select"
-            value={occupationValue}
-            label="Occupation"
-            onChange={handleChange}
-          >
-            {occupationAndStateData.occupations.map((occupation: string) => {
-              return <MenuItem value={occupation}>{occupation}</MenuItem>;
-            })}
-          </Select>
-        </FormControl>
-        <Autocomplete
-          disablePortal
-          value={stateValue}
-          onChange={(event: any, newValue: string | null) => {
-            event.preventDefault();
-            setStateValue(newValue);
-          }}
-          id="state"
-          fullWidth
-          options={occupationAndStateData.states.map(
-            (state: states) => state.name
-          )}
-          sx={{ mt: 3, mb: 2 }}
-          renderInput={(params) => <TextField {...params} label="State" />}
-        />{" "}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Submit Form
-        </Button>
-      </Box>
-    );
-  }
-};
 
+  return loading ? (
+    <CircularProgress />
+  ) : (
+    <Box component="form" onSubmit={handleSubmit} maxWidth="sm">
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="name"
+        label="Name"
+        name="name"
+        autoComplete="name"
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <FormControl fullWidth sx={{ mt: 2 }}>
+        <InputLabel id="occupation-select">Occupation</InputLabel>
+        <Select
+          labelId="occupation-select"
+          id="frontend-occupation-select"
+          value={occupationValue}
+          label="Occupation"
+          onChange={handleChange}
+        >
+          {occupationAndStateData.occupations.map((occupation: string) => {
+            return <MenuItem value={occupation}>{occupation}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+      <Autocomplete
+        disablePortal
+        value={stateValue}
+        onChange={(event: any, newValue: string | null) => {
+          event.preventDefault();
+          setStateValue(newValue);
+        }}
+        id="state"
+        fullWidth
+        options={occupationAndStateData.states.map(
+          (state: states) => state.name
+        )}
+        sx={{ mt: 3, mb: 2 }}
+        renderInput={(params) => <TextField {...params} label="State" />}
+      />{" "}
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Submit Form
+      </Button>
+    </Box>
+  );
+};
 export default FormPage;
